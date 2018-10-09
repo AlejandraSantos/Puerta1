6b2c27fca43f393a643ec397e94fb18c6b88bb86
AlejandraSantos <32527499+AlejandraSantos@users.noreply.github.com>
2018-10-09 13:01:45 -0500

    Update PruebaP.ino

--- PruebaP.ino
+++ PruebaP.ino
@@ -1,24 +1,42 @@
-int buttonPin = 3;
-int ledPin = 13;
-int buttonState = 0;
-const int intPin = 2;
-volatile int state = LOW;
+#include <avr/sleep.h>
+int buz=11;
+int reed=3;
+int led=12;
+volatile int estado1= LOW;
 
-void setup() {
-pinMode(ledPin, OUTPUT);
-pinMode(buttonPin, INPUT);
-attachInterrupt(digitalPinToInterrupt(intPin), blink, RISING);
+void setup(){
+  pinMode(buz, OUTPUT);
+  pinMode(led,OUTPUT);
+  pinMode(reed, INPUT);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  attachInterrupt(digitalPinToInterrupt(reed), interrupcion, RISING);
+  Serial.begin(9600);
 }
 
 void loop(){
-buttonState = digitalRead( buttonPin);
-if (buttonState == HIGH) {
-    digitalWrite(ledPin, HIGH);}
-else {
-    digitalWrite(ledPin, LOW);
-    }
-} 
-void blink() {
-    state = !state;
-    digitalWrite(ledPin, state);
+  if(estado1==LOW){
+     digitalWrite(led,LOW);
+     sleep_enable();
+     sleep_mode();
+     digitalWrite(led,LOW);
+  }else{
+     sleep_disable();
+     digitalWrite(led,HIGH);
+     alarma(estado1);
+  }
+   
+}
+
+void interrupcion() {
+  estado1 = !estado1;
+}
+
+
+void alarma(int estado){
+  if (estado1 == LOW) {
+  } else {
+    int volumen=0;
+    analogWrite(buz, volumen); 
+    digitalWrite(buz, LOW);
+  }
 }
